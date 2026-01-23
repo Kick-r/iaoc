c4455308f4779e4996a9c59cf675929cd5bdb307
Kick-r <hackick21@gmail.com>
2026-01-23 14:01:29 -0300

    Adicionando a resposividade

--- db.py
+++ db.py
@@ -59,3 +59,5 @@ class Message(Base):
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+###eu gostaria de lhe perguntar uma coisa. Oh pai. O render, quando o site fica inativo por algumas horas, ele meio que fecha, ele fica inativo e aí quando você acessa o site novamente, ele meio que reinicia. Então, tipo, todos os dados do banco é perdido. Por que isso que eu tô achando, porque tipo, eu tentei acessar, ontem eu criei uma senha com o meu e-mail, aí hoje eu acessei, coloquei minha senha e não tava entrando. Tipo, é como se... E como eu acho isso? Porque eu criei outro usuário com o mesmo e-mail e falei ainda eu fiz existe isso, já é um usuário com o meu e-mail, entendeu? Ele não ia deixar. Me responde isso aí, amigos, de tudo.
